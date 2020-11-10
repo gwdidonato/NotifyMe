6a9782956981b2f2f8659bd0e64a228350964c4e
Guido Walter Di Donato <gw.didonato@gmail.com>
2020-11-10 22:14:30 +0100

    Pushed NotifyMe script

---
+++ notify_me.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+host_name=$(</proc/sys/kernel/hostname)
+
+#Executing the command
+start_time="$(date)"
+$*
+end_time="$(date)"
+
+#Setting notification mail content
+to_address="example@google.com"
+subject="Notification: Job Done @$host_name"
+mail_text="Subject: $subject
+
+Your job is done!
+
+Job: $*
+Host: @$host_name
+Start Time: $start_time
+End Time: $end_time
+
+Cheers!
+"
+
+#Sending the email
+echo "$mail_text" > email.txt
+sendmail $to_address < email.txt
+rm email.txt
